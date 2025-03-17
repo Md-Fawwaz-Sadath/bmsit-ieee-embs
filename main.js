e2a9a25a76b23954ad04b0d7efb46c7fae156179
mdfawwaz03 <mdfawwaz03@gmail.com>
2025-03-17 16:53:41 +0000

    changes made

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
-    //change navbar styles on scroll
-
-    window.addEventListener('scroll', () => {
-        document.querySelector('nav').classList.toggle
-        ('window-scroll', window.scrollY > 0)
-    })
-
-
-
-
-    const faqs = document.querySelectorAll('.faq');
-
-    faqs.forEach(faq => {
-        faq.addEventListener('click', () => {
-            faq.classList.toggle('open');
-
-
-            const icon = faq.querySelector('.faq__icon i');
-            if(icon.className === 'fa-duotone fa-solid fa-plus') {
-                icon.className = 'fa-duotone fa-solid fa-minus'
-            } else {
-                icon.className = 'fa-duotone fa-solid fa-plus'
-            }
-        })
-    })
-
-    // Mobile Menu Toggle
+// Mobile Menu Toggle
 const menu = document.querySelector('.nav_menu');
 const openBtn = document.querySelector('#open-menu-btn');
 const closeBtn = document.querySelector('#close-menu-btn');
 
+// Toggle menu
 openBtn.addEventListener('click', () => {
     menu.classList.add('active');
     closeBtn.style.display = 'inline-block';
@@ -39,4 +14,25 @@ closeBtn.addEventListener('click', () => {
     menu.classList.remove('active');
     closeBtn.style.display = 'none';
     openBtn.style.display = 'inline-block';
+});
+
+// Change navbar style on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // Change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'fa-duotone fa-solid fa-plus') {
+            icon.className = 'fa-duotone fa-solid fa-minus';
+        } else {
+            icon.className = 'fa-duotone fa-solid fa-plus';
+        }
+    });
 });
